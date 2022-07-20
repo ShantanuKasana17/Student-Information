090aded7487fdc34232dfe6d21e9a68f8159b789
Devesh Jain <deveshsolapur@gmail.com>
2022-07-20 20:32:00 +0530

    updated credits

--- app/src/main/java/com/example/studentinformation/MainActivity.java
+++ app/src/main/java/com/example/studentinformation/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+//Made by Devesh Jain 20BCE10613
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
